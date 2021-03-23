26b1aba7023a16f8f77d1a681d2df9cd2df8d345
doyeon353 <dusehrla6432@naver.com>
2021-03-23 22:23:49 +0900

    post list click event #4
    click event로 각각의 post의 이미지를 나타내고 없어지게 하는 것은 완료했지만 하나의 버튼을 눌러서 이미지를 나오게 한 후 다른버튼을 눌렀을떄 기존의 이미지를 지우고 클릭한 버튼의 이미지가 나오게 하는 것 을 구현하지 못했다

--- titleClick.js
+++ titleClick.js
@@ -4,18 +4,36 @@ const liPost3 = document.getElementById('lipost-3');
 const liPost4 = document.getElementById('lipost-4');
 const liPost5 = document.getElementById('lipost-5');
 
+const imgArray = ['post-1', 'post-2', 'post-3', 'post-4', 'post-5'];
+
+function toggleEvent(post) {
+    let postImg = document.getElementById(post);
+    if (postImg.classList.contains('remove')) {
+        postImg.classList.remove('remove');
+    }
+    postImg.classList.toggle('show');
+    if (!postImg.classList.contains('show')) {
+        postImg.classList.toggle('remove');
+    }
+}
+
+
+
 
 function printPost(event) {
     const target = event.target;
-    if (target.id === 'lipost-1') {
-        let postImg = document.getElementById('post-1');
-        if (postImg.classList.contains('remove')) {
-            postImg.classList.remove('remove');
-        }
-        postImg.classList.toggle('show');
-        if (!postImg.classList.contains('show')) {
-            postImg.classList.toggle('remove');
-        }
+    const tarId = target.id;
+    if (tarId === 'lipost-1') {
+        toggleEvent('post-1');
+
+    } else if (tarId === 'lipost-2') {
+        toggleEvent('post-2');
+    } else if (tarId === 'lipost-3') {
+        toggleEvent('post-3');
+    } else if (tarId === 'lipost-4') {
+        toggleEvent('post-4');
+    } else {
+        toggleEvent('post-5');
     }
 
 }
